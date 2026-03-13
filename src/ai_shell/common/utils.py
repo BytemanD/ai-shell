b170f1098d96f94a466a68c599544d86b89ad9e9
BytemanD <byteman.d@outlook.com>
2026-03-14 00:15:59 +0800

    feat: 删除冗余代码
    Signed-off-by: BytemanD <byteman.d@outlook.com>

--- src/ai_shell/common/utils.py
+++ src/ai_shell/common/utils.py
@@ -1,34 +1,5 @@
-import platform
 import re
-import subprocess
-import sys
-from typing import List, Sequence
-
-
-class BashExecutor:
-    CMD = "bash"
-
-    def _execute_cmd(self, command: str) -> Sequence[str]:
-        return ["bash", "-c", command]
-
-    def execute(self, command: str) -> tuple[int, str, str]:
-        cmd = self._execute_cmd(command)
-        subprocess.run(cmd, text=True, stdout=sys.stdout, stderr=sys.stderr)
-
-
-class WindowsExecutor(BashExecutor):
-    CMD = "powershell"
-
-    def _execute_cmd(self, command):
-        return ["powershell", "-Command", command]
-
-
-def get_exector() -> BashExecutor:
-    if platform.system() == "Windows":
-        return WindowsExecutor()
-    elif platform.system() == "Linux" or platform.system() == "Darwin":
-        return BashExecutor()
-    raise RuntimeError(f"Unsupported OS: {platform.system()}")
+from typing import List
 
 
 def find_code_blocks_from_markdown(markdown_text: str) -> List[str]:
@@ -43,5 +14,3 @@ def find_code_blocks_from_markdown(markdown_text: str) -> List[str]:
         if code_blocks:
             return code_blocks
     return []
-
-
